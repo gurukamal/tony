6d72ca5ac563dfc09b3ee63f95bcb10a66e2fb8f
gurukamal <info.gurukamalchawla@gmail.com>
2019-04-05 11:07:23 +0530

    updated app

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends React.Component{
 			</div>
 		    <div className="list">
 			    <ul className="list-content">
-				<li className="matter"> <a href="">Mission</a>
+				<li className="matter"> <a href="">Mission <i class="fa fa-angle-down"></i></a>
 				<ul className="drop">
 				<li>Contribution</li>
 				<li>About Tony Robbins</li>
@@ -17,13 +17,13 @@ class App extends React.Component{
 				<li>Ask Tony Anything</li>
 				</ul>
 				</li>
-				<li className="matter"> <a href="">Store</a>
+				<li className="matter"> <a href="" className="btn">Store <i class="fa fa-angle-down"></i></a>
 				<ul className="drop">
 				<li>All Product</li>
 				<li>Training System</li>
 				</ul>
 				</li>
-				<li className="matter"><a href="">Experiences</a>
+				<li className="matter"><a href="" className="btn">Experiences <i class="fa fa-angle-down"></i></a>
 				<ul className="drop">
 				<li >All Experiences</li>
 				<li>Unleash The Power Wiyh-in</li>
@@ -33,11 +33,11 @@ class App extends React.Component{
 				<li>Life mastery</li>
 				<li>Premium</li>
 				</ul></li>
-				<li className="matter"> <a href="">Coaching</a>
+				<li className="matter"> <a href="" className="btn">Coaching <i class="fa fa-angle-down"></i></a>
 				<ul className="drop">
 				<li>Results Coaching</li>
 				</ul></li>
-				<li className="matter"> <a href="">Resources</a>
+				<li className="matter"> <a href="" className="btn">Resources <i class="fa fa-angle-down"></i></a>
 				<ul className="drop">
 				<li>All Experiences</li>
 				<li>Unleash The Power With-in</li>
@@ -48,6 +48,9 @@ class App extends React.Component{
 				<li>Premium</li>
 				</ul></li>
 				</ul>
+			</div>
+			<div className="search">
+				<i class="fa fa-search"></i>
 			</div>
 			</div>
 		);
